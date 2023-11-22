ef57e091077336236d1dd82850c97207da22cff0
foxmon <jjockrod@naver.com>
2023-11-22 16:35:56 +0900

    router

---
+++ src/routes/Private/PrivateRouter.tsx
@@ -0,0 +1,43 @@
+import { useEffect, Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+
+// apis
+import { get } from "@/apis";
+
+// utils
+import { AUTH_KEY, SessionStorage } from "@/utils";
+
+// types
+import { Nullable, Auth, ChildrenAlias } from "@/types";
+
+interface PrivateRouterProps {
+  children: ChildrenAlias;
+}
+
+export const PrivateRouter = ({ children }: PrivateRouterProps) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      const authToken: Nullable<string> = SessionStorage.getItem(AUTH_KEY);
+
+      if (!authToken) {
+        navigate("/");
+      }
+
+      const authCheck: Auth = await get("/user/me", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer: ${authToken}`,
+        },
+      });
+
+      if (!authCheck._id) {
+        navigate("/");
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <Fragment>{children}</Fragment>;
+};
