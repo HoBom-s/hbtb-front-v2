17750ae1d874b1f3be2e06c9839c5c50a186b930
foxmon <jjockrod@naver.com>
2023-12-01 18:19:10 +0900

    article

--- src/components/Articles/Fetch/ArticleDetailFetch.tsx
+++ src/components/Articles/Fetch/ArticleDetailFetch.tsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 // chakra
 import { Box, Heading, Text } from "@chakra-ui/react";
 
 // hooks
-import { useFetch } from "@/hooks";
+import { useFetch, useModal } from "@/hooks";
 
 // components
-import { ArticleDetail } from "@/components";
+import { ArticleDetail, CommonConfirmModal, CommonModal } from "@/components";
 
 // apis
-import { get } from "@/apis";
+import { del, get } from "@/apis";
+
+// utils
+import { SessionStorage, AUTH_KEY } from "@/utils";
 
 // types
 import type { Nullable, Article } from "@/types";
@@ -22,6 +28,40 @@ export const ArticleDetailFetch = ({ path }: ArticleDetailFetchProps) => {
     get,
     `/article/find/${path}`,
   );
+  const { isModalOpen, handleModalOpenStateChange } = useModal();
+
+  const [articleId, setArticleId] = useState<string>("");
+
+  const navigate = useNavigate();
+
+  const handleUpdateButtonClick = (_id: string) => {
+    handleModalOpenStateChange();
+  };
+
+  const handleDeleteButtonClick = (_id: string) => {
+    setArticleId(_id);
+
+    handleModalOpenStateChange();
+  };
+
+  const handleModalOkButtonClick = async () => {
+    if (articleId) {
+      const deletedId: Nullable<string> = await del(
+        `/article/delete/${articleId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${SessionStorage.getItem(AUTH_KEY)}`,
+          },
+        },
+      );
+
+      if (deletedId) {
+        setArticleId("");
+
+        navigate("/");
+      }
+    }
+  };
 
   return (
     <Box w="100%" h="100%" py="24px">
@@ -35,13 +75,29 @@ export const ArticleDetailFetch = ({ path }: ArticleDetailFetchProps) => {
             </Text>
           </Box>
           <ArticleDetail
+            _id={articleContentsResult._id}
             contents={articleContentsResult.contents}
             authorThumbnail={articleContentsResult.writers[0].profileImg}
             authorNickname={articleContentsResult.writers[0].nickname}
             authorIntroduction={articleContentsResult.writers[0].introduction}
+            onUpdateButtonClickEvent={handleUpdateButtonClick}
+            onDeleteButtonClickEvent={handleDeleteButtonClick}
           />
         </Box>
       )}
+      <CommonConfirmModal
+        isOpen={isModalOpen}
+        title="WARNING"
+        contents="Are you sure delete this article ?"
+        onModalOkButtonClickEvent={handleModalOkButtonClick}
+        onModalCloseButtonClickEvent={handleModalOpenStateChange}
+      />
+      <CommonModal
+        isOpen={isModalOpen}
+        title="WARNING"
+        bodyContents="Not implemented..."
+        onModalCloseButtonClickEvent={handleModalOpenStateChange}
+      />
     </Box>
   );
 };
